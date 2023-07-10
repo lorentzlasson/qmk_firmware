2e280af2948906ba75bbde527f0ae73d4fb47161
Lorentz Lasson <lasson.lorentz@gmail.com>
2023-07-10 21:23:12 +0200

    Align with upstream

--- keyboards/crkbd/keymaps/lorentzlasson/config.h
+++ keyboards/crkbd/keymaps/lorentzlasson/config.h
@@ -33,7 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
 #define PREVENT_STUCK_MODIFIERS
-#define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
