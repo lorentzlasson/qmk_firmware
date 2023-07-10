5260da2d7dde0a9ae959e0dfd90bb9a7350eae82
Lorentz Lasson <lasson.lorentz@gmail.com>
2023-07-10 21:46:42 +0200

    Remove unused config

--- keyboards/crkbd/keymaps/lorentzlasson/config.h
+++ keyboards/crkbd/keymaps/lorentzlasson/config.h
@@ -34,13 +34,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define PREVENT_STUCK_MODIFIERS
 #define PERMISSIVE_HOLD
-
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
